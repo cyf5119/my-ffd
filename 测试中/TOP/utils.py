7279f3b3cc66c3d889fb710d91207b406c3b1815
CYF <89454904+cyf2023@users.noreply.github.com>
2023-09-02 16:46:02 +0800

    Create utils.py

---
+++ 测试中/TOP/utils.py
@@ -0,0 +1,39 @@
+import logging
+import glm
+import math
+import threading
+import time
+
+from raid_helper import utils as raid_utils
+from raid_helper.utils.typing import *
+from raid_helper.data import special_actions
+from raid_helper.data import omen_color
+
+top = raid_utils.MapTrigger(1122)
+center = glm.vec3(100, 0, 100)
+logger = logging.getLogger('raid_helper/top')
+
+class col():
+    red_line = glm.vec4(1, .2, .2, .8)
+    red_surface = glm.vec4(1, .6, .6, .2)
+    green_line = glm.vec4(.2, 1, .2, .8)
+    green_surface = glm.vec4(.6, 1, .6, .2)
+    blue_line = glm.vec4(.2, .2, 1, .8)
+    blue_surface = glm.vec4(.6, .6, 1, .2)
+
+    lightblue_line = glm.vec4(.2, 1, 1, .8)
+    lightblue_surface = glm.vec4(.6, 1, 1, .2)
+    purple_line = glm.vec4(1, .2, 1, .8)
+    purple_surface = glm.vec4(1, .6, 1, .2)
+    yellow_line = glm.vec4(1, 1, .2, .8)
+    yellow_surface = glm.vec4(1, 1, .6, .2)
+
+    orange_line = glm.vec4(1, .6, .2, .8)
+    orange_surface = glm.vec4(1, .8, .6, .2)
+
+class The_Omega_Protocol():
+
+    p1_Program_Loop_priority = top.add_value(raid_utils.Select(
+        '绝欧米茄/优先级设置/P1-循环程序', [('HTDH', 'h1mtstd1d2d3d4h2'),('THD', 'mtsth1h2d1d2d3d4'),], 'h1mtstd1d2d3d4h2'))
+#    p1_Pantokrator_priority = top.add_value(raid_utils.Select( # 这个目前还没屁用，先写上再说
+#        '绝欧米茄/优先级设置/P1-全能之主', [('HTDH', 'h1mtstd1d2d3d4h2'),('THD', 'mtsth1h2d1d2d3d4'),], 'h1mtstd1d2d3d4h2'))
