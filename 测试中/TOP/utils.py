893d4ab0fe6bb6a3503f910dcfd3c241e928353a
CYF <89454904+cyf2023@users.noreply.github.com>
2023-09-04 19:41:28 +0800

    Update utils.py

--- 测试中/TOP/utils.py
+++ 测试中/TOP/utils.py
@@ -43,3 +43,5 @@ class The_Omega_Protocol():
         '绝欧米茄/优先级设置/P2.5-速射式波动炮', [('HTDH', 'h1mtstd1d2d3d4h2'),('THD', 'mtsth1h2d1d2d3d4'),], 'h1mtstd1d2d3d4h2'))
     p3_Oversampled_Wave_Cannon_priority = top.add_value(raid_utils.Select(
         '绝欧米茄/优先级设置/P3-探测式波动炮', [('HTDH', 'h1mtstd1d2d3d4h2'),('THD', 'mtsth1h2d1d2d3d4'),], 'h1mtstd1d2d3d4h2'))
+    p4_priority = top.add_value(raid_utils.Select(
+        '绝欧米茄/优先级设置/P4-由远到近', [('坦克远程奶妈近战', 'mtstd3d4h1h2d1d2'),('坦克远程近战奶妈', 'mtstd3d4d1d2h1h2'),], 'mtstd3d4h1h2d1d2'))
